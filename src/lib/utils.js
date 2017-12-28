0278a1cacbf6a3a2d43d32c5c78042bfd020e5d1
孙昊翔 <673686754@qq.com>
2017-12-28 17:00:07 +0800

    no message

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,27 @@
 let local = {
   test () {
     console.log('test')
+  },
+  clone (obj) {
+    var o
+    if (typeof obj === 'object') {
+      if (obj === null) {
+        o = null
+      } else {
+        if (obj instanceof Array) {
+          o = []
+          for (var i = 0, len = obj.length; i < len; i++) {
+            o.push(local.clone(obj[i]))
+          }
+        } else {
+          o = {}
+          for (var j in obj) {
+            o[j] = local.clone(obj[j])
+          }
+        }
+      }
+    } else { o = obj }
+    return o
   }
 }
 
