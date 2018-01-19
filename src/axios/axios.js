efc9d132aa67c32d30a33033db1e49b0e2cd98eb
孙昊翔 <673686754@qq.com>
2018-01-19 22:28:00 +0800

    no message

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,8 +5,12 @@ import Cookies from 'js-cookie'
 axios.defaults.baseURL = 'http://localhost:9999/'
 
 // get 方法
-function get (url) {
-  return body => axios.get(url, { params: {
+// function get (url) {
+//   return body => axios.get(url, {params: body})
+// }
+
+function getWithToken (url) {
+  return body => axios.get(url, {params: {
     ...body,
     token: Cookies.get('bearcToken')
   }})
@@ -14,6 +18,10 @@ function get (url) {
 
 // post 方法
 function post (url) {
+  return body => axios.post(url, body)
+}
+
+function postWithToken (url) {
   return body => axios.post(url, {
     ...body,
     token: Cookies.get('bearcToken')
@@ -38,13 +46,13 @@ export const register = post('/register')  // 注册
 export const login = post('/login')  // 登录
 
 // 后台
-export const verifyToken = post('users/verifytoken')  // 验证登录
-export const upload = post('users/upload')  // 上传文件
-export const goodsList = get('users/goods/goodslist')  // 商品列表
-export const addGood = post('users/goods/addgood')  // 添加商品
-export const modifyGood = post('users/goods/modifygood')  // 修改商品
-export const removeGood = post('users/goods/removegood')  // 删除商品
-export const typesList = get('users/types/typeslist')  // 分类列表
-export const addType = post('users/types/addtype')  // 添加分类
-export const modifyType = post('users/types/modifytype')  // 修改分类
-export const removeType = post('users/types/removetype')  // 删除分类
+export const verifyToken = postWithToken('users/verifytoken')  // 验证登录
+export const upload = postWithToken('users/upload')  // 上传文件
+export const goodsList = getWithToken('users/goods/goodslist')  // 商品列表
+export const addGood = postWithToken('users/goods/addgood')  // 添加商品
+export const modifyGood = postWithToken('users/goods/modifygood')  // 修改商品
+export const removeGood = postWithToken('users/goods/removegood')  // 删除商品
+export const typesList = getWithToken('users/types/typeslist')  // 分类列表
+export const addType = postWithToken('users/types/addtype')  // 添加分类
+export const modifyType = postWithToken('users/types/modifytype')  // 修改分类
+export const removeType = postWithToken('users/types/removetype')  // 删除分类
