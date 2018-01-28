0ce09539fca6edcf7f132911b2fa9642aff9cd50
孙昊翔 <673686754@qq.com>
2018-01-28 21:42:52 +0800

    no message

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -68,10 +68,6 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
       }
     ]
   }
